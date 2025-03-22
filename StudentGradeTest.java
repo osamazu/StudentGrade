b51bfb5a5c2d7ba64ca2b49775942160383757b1
osamazu <osamah8819@gmail.com>
2025-03-22 20:57:28 +0300

    Add files via upload
    Updated StudentGradeTest.java to JUnit 5

---
+++ StudentGradeTest.java
@@ -0,0 +1,34 @@
+package studentgrade;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentGradeTest {
+
+    @Test
+    void testGetGrade95() {
+        System.out.println("getGrade 95");
+        int mark = 95;
+        String expResult = "A";
+        String result = StudentGrade.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    void testGetGrade85() {
+        System.out.println("getGrade 85");
+        int mark = 85;
+        String expResult = "B";
+        String result = StudentGrade.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    void testGetGrade75() {
+        System.out.println("getGrade 75");
+        int mark = 75;
+        String expResult = "F";
+        String result = StudentGrade.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+}
